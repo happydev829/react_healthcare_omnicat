104912b34722243e380bcfa0bea9834f9d2d48ed
mrdev126 <luckman126work@gmail.com>
2018-08-09 16:05:53 -0500

    keep new component/App

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from 'components/Header'
+import Main from 'components/Main'
+
+
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+    <Header />
+    <Main />
+  </div>
+)
+
+export default App
