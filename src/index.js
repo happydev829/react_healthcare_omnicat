a96cab22a075216f5099c8e8fdcbfbd98b300561
mrdev126 <luckman126work@gmail.com>
2018-08-09 15:35:42 -0500

    audit & fix dependency issues

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import registerServiceWorker from './utils/registerServiceWorker'
+import 'index.css'
+import registerServiceWorker from 'utils/registerServiceWorker'
 // import App from 'components/App'
-import App from './App'
+// import App from './App'
+import App from 'components/App'
 
 render((
   <BrowserRouter>
