57ba9330618b17a256945dde96bdfb09db9fb1d2
mrdev126 <luckman126work@gmail.com>
2018-08-09 22:59:02 -0500

    progress with jest

--- scripts/test.js
+++ scripts/test.js
@@ -5,9 +5,10 @@ process.env.PUBLIC_URL = ''
 // if this file is missing. dotenv will never modify any environment variables
 // that have already been set.
 // https://github.com/motdotla/dotenv
-require('dotenv').config({silent: true});
+require('dotenv').config({silent: true, path: '.env.test'});
 
 const jest = require('jest');
+
 const argv = process.argv.slice(2);
 
 // Watch unless on CI or in coverage mode
