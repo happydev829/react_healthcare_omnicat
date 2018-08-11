e115328d70c3d4c56923a77996b63919a429f065
mrdev126 <luckman126work@gmail.com>
2018-08-11 04:00:06 -0500

    remove experimental workflow scripts for now

--- coverage/App.test.js
+++ coverage/App.test.js
@@ -1,23 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from '../src/App'
-import request from 'request'
-it('renders without crashing', () => {
+
+test('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
 })
 
-it('has storageValue && web3 properties', () => {
+test('has storageValue && web3 properties', () => {
   const newapp = new App()
-  // expect(newapp.hasOwnProperty('storageValue')) == true
-  // expect(request('').then(() => {return newapp.getState('web3')} )).toBeTruthy()
+  expect(newapp.state.hasOwnProperty('storageValue')).toBe(true)
+  expect(newapp.state.hasOwnProperty('web3')).toBe(true)
 })
-
-// it('gets web3', () => {
-//   const newapp = new App()
-//   // expect(newapp.web3).toBe(!null)
-//   // const app = new App()
-//   // expect(app.hasOwnProperty('web3')).toBe(true)
-//   // expect(app.).toBe(true)
-//   // typeof(a) === App
-// })
