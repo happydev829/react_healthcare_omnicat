805f3ba1053266304eb46e3bb202d7aeaa6907db
happydev829 <happydev000@gmail.com>
2018-08-11 11:58:56 -0500

    routing and package woes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
-const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/dass'>DASS42</Link></li>
-        <li><Link to='/iron'>Iron Levels</Link></li>
-      </ul>
-    </nav>
-  </header>
-)
-
+class Header extends React.Component{
+  render() {
+    return (
+      <header>
+        <nav>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/dass'>DASS42</Link></li>
+            <li><Link to='/iron'>Iron Levels</Link></li>
+          </ul>
+        </nav>
+      </header>
+    )
+  }
+}
 export default Header
