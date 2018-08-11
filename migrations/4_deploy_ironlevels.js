2420001b6a961672d0c012095a9b3c9e1608b986
mrdev126 <luckman126work@gmail.com>
2018-08-11 04:58:47 -0500

    deploy current contracts for dev

---
+++ migrations/4_deploy_ironlevels.js
@@ -0,0 +1,5 @@
+var IronLevels = artifacts.require("./IronLevels.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(IronLevels);
+};
---
+++ migrations/5_deploy_dass42.js
@@ -0,0 +1,5 @@
+var Dass42 = artifacts.require("./Dass42.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(Dass42);
+};
