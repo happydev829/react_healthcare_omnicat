6ebecd3dab565c6be73d56f7a4bbdffc29ce404b
mrdev126 <luckman126work@gmail.com>
2018-08-12 12:53:51 -0500

    pause, test passes

--- coverage/App.test.js
+++ coverage/App.test.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import App from '../src/App';
+import App from '../src/components/App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div)
+});
 
 test('has storageValue && web3 properties', () => {
   const newapp = new App()
   expect(newapp.state.hasOwnProperty('storageValue')).toBe(true)
   expect(newapp.state.hasOwnProperty('web3')).toBe(true)
 })
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-
-  // const StoreInstance = Store();
-
-  ReactDOM.render((
-     // <Provider store={StoreInstance}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-     // </Provider>,
-  ), div)
-});
-
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// // import  from 'react-router-dom'
-// import App from '../src/App'
-//
-// test('renders without crashing', () => {
-//   const div = document.createElement('div')
-//   // const toro = document.getElementById('root')
-//   ReactDOM.render( <App />, div )
-//
-// })
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,10 +1,17 @@
-// 
-//
-// <BrowserRouter>
-//   <Switch>
-//     <Route exact path='/' component={Home}/>
-//     <Route exact path='/dass' component={Dass42}/>
-//     <Route exact path='/iron' component={IronLevels}/>
-//     <Route path='*' component={NotFound}/>
-//   </Switch>
-// </BrowserRouter>
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Dass42 from './Dass42'
+import IronLevels from './IronLevels'
+import Home from './Home'
+import NotFound from './NotFound'
+const Router = () => (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/dass' component={Dass42}/>
+          <Route exact path='/iron' component={IronLevels}/>
+          <Route path='*' component={NotFound}/>
+        </Switch>
+      </BrowserRouter>
+)
+export default Router
