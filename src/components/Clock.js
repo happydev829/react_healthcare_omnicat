fed551f6179fa5c779ae4e80c1c6b342df3ad3a4
mrdev126 <luckman126work@gmail.com>
2018-08-14 15:39:46 -0500

    little cleanup

--- src/components/Clock.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-export default class Clock extends React.Component {
-    render() {
-      return (
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
-      )
-    }
-  }
