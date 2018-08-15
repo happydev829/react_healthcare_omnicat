0c5b0c5c24c279f3015cfb580d9bb47ff32f4f5f
happydev829 <happydev000@gmail.com>
2018-08-15 07:03:05 -0500

    show stringified props for contract definitions & instances :)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
 import Dass42 from './Dass42'
@@ -15,25 +15,33 @@ import Wellness from './Wellness'
 import NotFound from './NotFound'
 import './../css/Main.css'
 
-const Main = () => (
-  <main className="pure-u pure-u-md-3-4">
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/dass' component={Dass42}/>
-      <Route exact path='/iron' component={IronLevels}/>
-      <Route exact path='/aesthetics' component={Aesthetics}/>
-      <Route exact path='/about' component={About}/>
-      <Route exact path='/biochemistry' component={Biochemistry}/>
-      <Route exact path='/brain-health' component={BrainHealth}/>
-      <Route exact path='/crowdsale' component={Crowdsale}/>
-      <Route exact path='/hormones' component={Hormones}/>
-      <Route exact path='/physical-fitness' component={PhysicalFitness}/>
-      <Route exact path='/risk-and-prevention' component={RiskAndPrevention}/>
-      <Route exact path='/wellness' component={Wellness}/>
-      <Route path='*' component={NotFound}/>
-    </Switch>
-    <hr/>
-  </main>
-)
+class Main extends Component {
+  render() {
+    return (
+      <main className="pure-u pure-u-md-3-4">
+        <div className="pure-u pure-u-1">
+          ( this.props: { JSON.stringify({...this.props}) } )
+          <br/>
+        </div>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/dass' component={Dass42}/>
+          <Route exact path='/iron' component={IronLevels}/>
+          <Route exact path='/aesthetics' component={Aesthetics}/>
+          <Route exact path='/about' component={About}/>
+          <Route exact path='/biochemistry' component={Biochemistry}/>
+          <Route exact path='/brain-health' component={BrainHealth}/>
+          <Route exact path='/crowdsale' component={Crowdsale}/>
+          <Route exact path='/hormones' component={Hormones}/>
+          <Route exact path='/physical-fitness' component={PhysicalFitness}/>
+          <Route exact path='/risk-and-prevention' component={RiskAndPrevention}/>
+          <Route exact path='/wellness' component={Wellness}/>
+          <Route path='*' component={NotFound}/>
+        </Switch>
+        <hr/>
+      </main>
+    )
+  }
+}
 
 export default Main
