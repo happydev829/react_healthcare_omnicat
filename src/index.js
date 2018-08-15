d5b4a57fe9ba952695bcc0770f0a90ecd57558af
mrdev126 <luckman126work@gmail.com>
2018-08-15 14:48:36 -0500

    test prep

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import registerServiceWorker from './utils/registerServiceWorker'
 import App from './components/App'
 
