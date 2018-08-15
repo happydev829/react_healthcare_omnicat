89fc6b766d80d3378f67d6dbdc2a75117330905c
happydev829 <happydev000@gmail.com>
2018-08-15 14:48:36 -0500

    test prep

--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -20,7 +20,14 @@ let getWeb3 = new Promise(function(resolve, reject) {
     } else {
       // Fallback to localhost if no web3 injection. We've configured this to
       // use the development console's port by default.
-      var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545')
+      var provider = null;
+
+      // Ganache GUI
+      provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545/m/44'/60'/0'/0/1")
+      // Ganache CLI
+      provider = provider ? provider : new Web3.providers.HttpProvider("http://127.0.0.1:8545/m/44'/60'/0'/0/1")
+      // Truffle
+      provider = provider ? provider : new Web3.providers.HttpProvider("http://127.0.0.1:9545")
 
       web3 = new Web3(provider)
 
@@ -28,7 +35,7 @@ let getWeb3 = new Promise(function(resolve, reject) {
         web3: web3
       }
 
-      console.log("No web3 instance injected, using localhost:9545's web3.");
+      console.log(`No web3 instance injected, using ${provider.host}'s web3.`);
 
       resolve(results)
     }
--- truffle.js
+++ truffle.js
@@ -2,13 +2,32 @@ var env = require('dotenv').config({ path: '.env' })
 
 const HDWalletProvider = require('truffle-hdwallet-provider')
 module.exports = {
-  networks: {
+  networks: { // truffle develop --network dev
     dev: {
       host: 'localhost',
       port: 9545,
       network_id: '*',
       provider: function() {
-        return new HDWalletProvider(env.TRUFFLE_DEV_MNEMONIC, 'http://127.0.0.1:9545')
+        return new HDWalletProvider(
+          env.TRUFFLE_DEV_MNEMONIC, 'http://127.0.0.1:9545/')
+      }
+    },
+    gcli: { // tab1: ganache-cli  & tab2: truffle console --network gcli
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      provider: function() {
+        return new HDWalletProvider(
+          env.GCLI_MNEMONIC, 'http://127.0.0.1:8545/') // m/44\'/60\'/0\'/0/1'
+      }
+    },
+    ganache: { // run Ganache app and 'truffle console --network ganache'
+      host: 'localhost',
+      port: 7545,
+      network_id: '5777',
+      provider: function() {
+        return new HDWalletProvider(
+          env.GANACHE_APP_MNEMONIC, "http://127.0.0.1:7545/") //m/44'/60'/0'/0/1") 
       }
     }
   }
