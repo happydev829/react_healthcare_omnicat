94429184a97c9e3009e30a2d78e49979ea08ed40
happydev829 <happydev000@gmail.com>
2018-08-18 21:40:37 -0500

    fiddle w/ migrations

--- migrations/2_deploy_omnicat.js
+++ migrations/2_deploy_omnicat.js
@@ -3,6 +3,7 @@ var OmniCAT = artifacts.require("./OmniCAT.sol");
 
 // Make sure Ownable migrates first since OmniCat inherits
 module.exports = function(deployer) {
-  deployer.deploy(Ownable);
-  deployer.deploy(OmniCAT);
+  deployer.deploy(Ownable).then((ownableInstance) => {
+    deployer.deploy(OmniCAT);
+  });
 };
