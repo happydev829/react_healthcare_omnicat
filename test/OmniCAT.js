33fad747bf7e0f4696cc628924d41edb9f273c7e
mrdev126 <luckman126work@gmail.com>
2018-08-22 23:08:33 -0500

    simplify truffle tests

--- test/OmniCAT.js
+++ test/OmniCAT.js
@@ -1,33 +1,29 @@
-const assert = require('assert');
-const contract = require('truffle-contract')
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
-const json = require('./../build/contracts/OmniCAT.json');
+// const assert = require('assert');
+// const contract = require('truffle-contract')
+// const ganache = require('ganache-cli');
+// const Web3 = require('web3');
+// const web3 = new Web3(ganache.provider());
+// const json = require('./../build/contracts/OmniCAT.json');
 // const _interface = json['abi'];
 // const bytecode = json['bytecode'];
 
-let accounts, omnicat, owner, omnicatInstance;
+var Ownable = artifacts.require("Ownable")
+var OmniCAT = artifacts.require("OmniCAT")
 
-beforeEach(async () => {
+var accounts, omnicat, owner;
 
-  accounts = await web3.eth.getAccounts((error, accounts) => {
-    owner = accounts[0];
-    return accounts;
-  });
+contract('OmniCAT', async (accounts) => {
+  it('deploys a contract', async () => {
+    let inst = await OmniCAT.deployed()
+    owner = inst.owner
+    omnicat = inst
+    assert(inst)
 
-  omnicat = contract(json)
-  omnicat.setProvider(web3.currentProvider)
-  omnicat.deployed().then((instance) => {
-      omnicatInstance = instance
-      // keys.owner = keys.omni.inst.owner.call()
-    }).then((result) => {
-      // Update state with the result.
-      omnicatInstance.send({ from: owner, gas: '1000000' });
-    })
-});
+  })
 
-describe('OmniCAT', () => {
+  it('gives web3 to the test env', async () => {
+    assert(web3)
+  })
 
   it('deploys a contract', async () => {
 
@@ -36,7 +32,5 @@ describe('OmniCAT', () => {
     assert.equal(owner, omnicatOwner,
       "The manager/owner is the one who launches the smart contract.")
   });
-
-  // Continue from this line from now on...
 
 });
