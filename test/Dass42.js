e2b84288f9a7f07bab2a18ef6d9c8db3177cb4c6
happydev829 <happydev000@gmail.com>
2018-08-22 23:08:33 -0500

    simplify truffle tests

--- test/Dass42.js
+++ test/Dass42.js
@@ -1,42 +1,36 @@
-const assert = require('assert');
-const contract = require('truffle-contract')
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
-const json = require('./../build/contracts/Dass42.json');
+// const assert = require('assert');
+// const contract = require('truffle-contract')
+// const ganache = require('ganache-cli');
+// const Web3 = require('web3');
+// const web3 = new Web3(ganache.provider());
+// const json = require('./../build/contracts/Dass42.json');
 // const _interface = json['abi'];
 // const bytecode = json['bytecode'];
 
-let accounts, dass42, owner, dass42Instance;
+var Ownable = artifacts.require("Ownable")
+var OmniCAT = artifacts.require("OmniCAT")
+var Dass42 = artifacts.require("Dass42")
 
-beforeEach(async () => {
+var accounts, dass42, owner;
 
-  accounts = await web3.eth.getAccounts((error, accounts) => {
-    owner = accounts[0];
-    return accounts;
-  });
-
-  dass42 = contract(json)
-  dass42.setProvider(web3.currentProvider)
-  dass42.deployed().then((instance) => {
-      dass42Instance = instance
-      // keys.owner = keys.omni.inst.owner.call()
-    }).then((result) => {
-      // Update state with the result.
-      dass42Instance.send({ from: owner, gas: '1000000' });
-    })
-});
-
-describe('Dass42', () => {
+contract('Dass42', async (accounts) => {
 
   it('deploys a contract', async () => {
+    let inst = await Dass42.deployed()
+    owner = inst.owner
+    dass42 = inst
+    assert(inst)
+  })
 
+  it('gives web3 to the test env', async () => {
+    assert(web3)
+  })
+
+  it('deploys a contract', async () => {
     const dass42Owner = dass42.owner
 
     assert.equal(owner, dass42Owner,
       "The manager/owner is the one who launches the smart contract.")
   });
-
-  // Continue from this line from now on...
 
 });
--- test/Ownable.js
+++ test/Ownable.js
@@ -1,42 +1,19 @@
-const assert = require('assert');
-const contract = require('truffle-contract')
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
-const json = require('./../build/contracts/Ownable.json');
+// const assert = require('assert');
+// const contract = require('truffle-contract')
+// const ganache = require('ganache-cli');
+// const Web3 = require('web3');
+// const web3 = new Web3(ganache.provider());
+// const json = require('./../build/contracts/Ownable.json');
 // const _interface = json['abi'];
 // const bytecode = json['bytecode'];
 
-let accounts, ownable, owner, ownableInstance;
+var Ownable = artifacts.require("Ownable")
 
-beforeEach(async () => {
-
-  accounts = await web3.eth.getAccounts((error, accounts) => {
-    owner = accounts[0];
-    return accounts;
-  });
-
-  ownable = contract(json)
-  ownable.setProvider(web3.currentProvider)
-  ownable.deployed().then((instance) => {
-      ownableInstance = instance
-      // keys.owner = keys.omni.inst.owner.call()
-    }).then((result) => {
-      // Update state with the result.
-      ownableInstance.send({ from: owner, gas: '1000000' });
-    })
-});
-
-describe('Ownable', () => {
+contract('Ownable', async (accounts) => {
 
   it('deploys a contract', async () => {
+    let inst = await Ownable.deployed()
+    assert(inst)
 
-    const ownableOwner = ownable.owner
-
-    assert.equal(owner, ownableOwner,
-      "The manager/owner is the one who launches the smart contract.")
   });
-
-  // Continue from this line from now on...
-
-});
+})
