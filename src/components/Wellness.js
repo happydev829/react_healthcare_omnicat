92c68a0ba236c8b46b24b7f5ce6c4a5fd66ec3fd
mrdev126 <luckman126work@gmail.com>
2018-08-24 02:00:49 -0500

    rudimentary Wellness (Health Appraisal) page & questionaire form

--- src/components/Wellness.js
+++ src/components/Wellness.js
@@ -1,10 +1,71 @@
-
 import React from 'react'
+import './../css/Wellness.sass'
+let { log, error, warn, info } = console
 
-const Wellness = () => (
+export default class Wellness extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      a: null
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.validate = this.validate.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
 
-    <h2>Page for Wellness</h2>
-  
-)
+  handleChange(event) {
+    event.preventDefault()
+    this.setState({a: true})
+  }
 
-export default Wellness
+  handleSubmit(event) {
+    event.preventDefault()
+    return this.validate() ?
+      log('valid') && true :
+      error('nopp') && false
+  }
+
+  validate() {
+    return true
+  }
+// pure-form-[aligned, stacked] pure-group pure-control pure-control-group span.pure-form-message-inline
+  render() {
+    return (
+      <div className="wellness">
+        <h2>Wellness &amp; Health Appraisal</h2>
+        <hr />
+        <div className="description">
+          <p>Your answers to this health appraisal questionnaire will assist your practitioner in gaining information about your current symptoms and health concerns. Please answer all questions, in each section.</p>
+          <p>Circle the number which best describes the frequency or severity of your symptoms over the previous <b>month</b>, or answer the <b>yes</b> or <b>no</b> questions by circling the appropriate letter.</p>
+          <p>You may note that some questions are repeated throughout the questionnaire. We would appreciate it if you can answer <b>all</b> questions, as this will ensure the most accurate interpretation of your results. You may however leave a question blank if you are unsure of the answer.</p>
+        </div>
+      <fieldset>
+        <legend>Questionaire</legend>
+        <form className="pure-g pure-form wellness-form" onSubmit={this.handleSubmit}>
+          <div className="pure-control-group pure-u-md-2-5">
+            <label className="pure-control"> Label 1
+              <input type="text" className="pure-control" defaultValue="Lorem ipsum" onChange={this.handleChange} />
+            </label>
+            <br />
+            <label className="pure-control"> Label 2
+              <input type="text" className="pure-control" defaultValue="Lorem 2" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="pure-control-group pure-u-md-2-5">
+            <label className="pure-control"> Label 3
+              <input type="text" className="pure-control" defaultValue="Lorem 3" onChange={this.handleChange} />
+            </label>
+            <br />
+            <label className="pure-control"> Label 4
+              <input type="text" className="pure-control" defaultValue="Lorem 4" onChange={this.handleChange} />
+            </label>
+          </div>
+          <div className="pure-u-1">
+            <button type="submit" className="pure-button pure-button-primary pure-control">See Results</button>
+          </div>
+        </form>
+      </fieldset>
+      </div>
+    )
+  }
+}
