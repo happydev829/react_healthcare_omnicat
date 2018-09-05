4c35f330728b0c99e362566637047e6cf8360bc2
happydev829 <happydev000@gmail.com>
2018-09-04 20:34:54 -0500

    basic Iron Levels form results

--- src/components/IronLevels.js
+++ src/components/IronLevels.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import chalk from 'chalk'
 import './../css/IronLevels.sass'
 // import { contract } from "utils/ethereumSetup"
 let { log, error, info, warn, dir } = console
@@ -9,17 +10,21 @@ export default class IronOptimiser extends React.Component {
       serumIron: null,
       transferrinIBC: null,
       transferrinSaturation: null,
-      serumFerritinAssay: null
+      serumFerritinAssay: null,
+      results: null
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
     this.validate = this.validate.bind(this)
+    this.tallyResults = this.tallyResults.bind(this)
   }
 
   validate() {
     log(Object.keys(this.state), Object.values(this.state))
-    return ! Object.values(this.state)
-               .map((val) => typeof(val) === 'number').includes(false)
+    return ! [this.state.serumIron, this.state.transferrinIBC,
+                this.state.transferrinSaturation, this.state.serumFerritinAssay]
+              .map((val) => typeof(val) === 'number')
+              .includes(false)
   }
 
   handleChange(event) {
@@ -30,11 +35,45 @@ export default class IronOptimiser extends React.Component {
     event.preventDefault()
     if (this.validate()) {
       log('valid')
+      this.tallyResults()
       return true
     } else {
-      log('nopp')
+      alert("Please provide values for all 4")
       return false
     }
+  }
+
+  tallyResults() {
+    let si = this.state.serumIron, ti = this.state.transferrinIBC,
+          ts = this.state.transferrinSaturation, sfa = this.state.serumFerritinAssay
+    // NOTE call contract?
+    let report_si = (si === 20 ? "Optimal" :
+          si >= 15 && si <= 19 ? "Normal low" :
+          si >= 21 && si <= 25 ? "Normal high" :
+          si >= 10 && si <= 14 ? "Suboptimal low" :
+          si >= 26 && si <= 33 ? "Suboptimal high" :
+                       si < 10 ? "Abnormal low" : "Abnormal high") + ' Serum Iron levels'
+    let report_ti = (ti === 60 ? "Optimal" :
+          ti >= 55 && ti <= 59 ? "Normal low" :
+          ti >= 61 && ti <= 65 ? "Normal high" :
+          ti >= 45 && ti <= 54 ? "Suboptimal low" :
+          ti >= 66 && ti <= 70 ? "Suboptimal high" :
+                       ti < 45 ? "Abnormal low" : "Abnormal high") + ' TransferrinIBC levels'
+    let report_ts = (ts === 40 ? "Optimal" :
+          ts >= 35 && ts <= 39 ? "Normal low" :
+          ts >= 41 && ts <= 45 ? "Normal high" :
+          ts >= 16 && ts <= 34 ? "Suboptimal low" :
+          ts >= 46 && ts <= 50 ? "Suboptimal high" :
+                       ts < 16 ? "Abnormal low" : "Abnormal high" ) + ' Transferrin Saturation levels'
+    let report_sfa = (sfa === 150 ? "Optimal" :
+         sfa >= 130 && sfa <= 149 ? "Normal low" :
+         sfa >= 151 && sfa <= 180 ? "Normal high" :
+         sfa >= 20 && sfa <= 129  ? "Suboptimal low" :
+         sfa >= 181 && sfa <= 290 ? "Suboptimal high" :
+         sfa < 20 ? "Abnormal low" : "Abnormal high") + ' Serum Ferritin Assay levels'
+    this.setState({
+      results: {si: report_si, ti: report_ti, ts: report_ts, sfa: report_sfa }
+    })
   }
 
   render() {
@@ -45,18 +84,22 @@ export default class IronOptimiser extends React.Component {
         <h3> Please provide values for the following instances </h3>
         <form className="pure-form pure-form-stacked" onSubmit={this.handleSubmit}>
 
+          { this.state.results && <h4>{this.state.results.si}</h4> }
           <label htmlFor="serumIron"> Serum Iron
             <input type="number" min="0" max="999" id="serumIron" onChange={this.handleChange} />
           </label>
-
+          
+          { this.state.results && <h4>{this.state.results.ti}</h4> }
           <label htmlFor="transferrinIBC"> TransferrinIBC
             <input type="number" min="0" max="999" id="transferrinIBC" onChange={this.handleChange} />
           </label>
 
+          { this.state.results && <h4>{this.state.results.ts}</h4> }
           <label htmlFor="transferrinSaturation"> Transferrin Saturation
             <input type="number" min="0" max="999" id="transferrinSaturation" onChange={this.handleChange} />
           </label>
 
+          { this.state.results && <h4>{this.state.results.sfa}</h4> }
           <label htmlFor="serumFerritinAssay"> Serum Ferritin Assay
             <input type="number" min="0" max="999" id="serumFerritinAssay" onChange={this.handleChange} />
           </label>
