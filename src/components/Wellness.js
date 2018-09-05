241670fdcb6ccceaf81f9390a515c641ee4dae60
mrdev126 <luckman126work@gmail.com>
2018-09-05 03:50:38 -0500

    progress on wellness form

--- src/components/Wellness.js
+++ src/components/Wellness.js
@@ -32,6 +32,7 @@ export default class Wellness extends React.Component {
   }
 // pure-form-[aligned, stacked] pure-group pure-control pure-control-group span.pure-form-message-inline
   render() {
+
     return (
       <div className="wellness">
         <h2>Wellness &amp; Health Appraisal</h2>
@@ -42,89 +43,74 @@ export default class Wellness extends React.Component {
           <p>You may note that some questions are repeated throughout the questionnaire. We would appreciate it if you can answer <b>all</b> questions, as this will ensure the most accurate interpretation of your results. You may however leave a question blank if you are unsure of the answer.</p>
         </div>
         <fieldset>
-          <legend>Questionnaire</legend>
-          <form className="pure-form wellness-form" onSubmit={this.handleSubmit}>
-            <h3>SECTION 1: GASTROINTESTINAL</h3>
-            <h4>Section 1.1 Stomach: Hypoacidity</h4>
-            <h5>1. Indigestion</h5>
-            <label className="pure-radio">0<input type="radio" checked={this.state.response[0] === 0} onChange={this.handleChange} /></label>
-            <label className="pure-radio">1<input type="radio" checked={this.state.response[0] === 1} onChange={this.handleChange} /></label>
-            <label className="pure-radio">2<input type="radio" checked={this.state.response[0] === 2} onChange={this.handleChange} /></label>
-            <label className="pure-radio">3<input type="radio" checked={this.state.response[0] === 3} onChange={this.handleChange} /></label>
-            <h5>2. Excessive belching, burping</h5>
-            <label className="pure-radio">0<input type="radio" checked={this.state.response[1] === 0} onChange={this.handleChange} /></label>
-            <label className="pure-radio">1<input type="radio" checked={this.state.response[1] === 1} onChange={this.handleChange} /></label>
-            <label className="pure-radio">2<input type="radio" checked={this.state.response[1] === 2} onChange={this.handleChange} /></label>
-            <label className="pure-radio">3<input type="radio" checked={this.state.response[1] === 3} onChange={this.handleChange} /></label>
-            <h5>3. Bloating or fullness commencing during or shortly after a meal</h5>
-            <label className="pure-radio">0<input type="radio" checked={this.state.response[2] === 0} onChange={this.handleChange} /></label>
-            <label className="pure-radio">1<input type="radio" checked={this.state.response[2] === 1} onChange={this.handleChange} /></label>
-            <label className="pure-radio">2<input type="radio" checked={this.state.response[2] === 2} onChange={this.handleChange} /></label>
-            <label className="pure-radio">3<input type="radio" checked={this.state.response[2] === 3} onChange={this.handleChange} /></label>
-            <h5>4. Sensation of food sitting in stomach for a prolonged period after a meal</h5>
-            <label className="pure-radio">0<input type="radio" checked={this.state.response[3] === 0} onChange={this.handleChange} /></label>
-            <label className="pure-radio">1<input type="radio" checked={this.state.response[3] === 1} onChange={this.handleChange} /></label>
-            <label className="pure-radio">2<input type="radio" checked={this.state.response[3] === 2} onChange={this.handleChange} /></label>
-            <label className="pure-radio">3<input type="radio" checked={this.state.response[3] === 3} onChange={this.handleChange} /></label>
-            <h5>5. Bad breath</h5>
-            <label className="pure-radio">0<input type="radio" checked={this.state.response[4] === 0} onChange={this.handleChange} /></label>
-            <label className="pure-radio">1<input type="radio" checked={this.state.response[4] === 1} onChange={this.handleChange} /></label>
-            <label className="pure-radio">2<input type="radio" checked={this.state.response[4] === 2} onChange={this.handleChange} /></label>
-            <label className="pure-radio">3<input type="radio" checked={this.state.response[4] === 3} onChange={this.handleChange} /></label>
-            <h5>6. Loss of appetite, or nausea</h5>
-            <label className="pure-radio">0<input type="radio" checked={this.state.response[5] === 0} onChange={this.handleChange} /></label>
-            <label className="pure-radio">1<input type="radio" checked={this.state.response[5] === 1} onChange={this.handleChange} /></label>
-            <label className="pure-radio">2<input type="radio" checked={this.state.response[5] === 2} onChange={this.handleChange} /></label>
-            <label className="pure-radio">3<input type="radio" checked={this.state.response[5] === 3} onChange={this.handleChange} /></label>
-            <h5>7. History of anaemia</h5>
-            <label className="pure-radio">0<input type="radio" checked={this.state.response[6] === 0} onChange={this.handleChange} /></label>
-            <label className="pure-radio">1<input type="radio" checked={this.state.response[6] === 1} onChange={this.handleChange} /></label>
-            <label className="pure-radio">2<input type="radio" checked={this.state.response[6] === 2} onChange={this.handleChange} /></label>
-            <label className="pure-radio">3<input type="radio" checked={this.state.response[6] === 3} onChange={this.handleChange} /></label>
-            <h4>Section 1.2 Stomach: Hyperacidity</h4>
-            ["1. Stomach pain, burning or aching, 1-4 hours after eating", "2. Feeling hungry just an hour or two after eating", "3. Indigestion or heartburn from spicy or fatty food, citrus, alcohol, or caffeine", "4. Stomach discomfort or pain in response to strong emotions, thoughts, or smell of food", "5. Heartburn aggravated by lying down or bending forward", "6. Antacids, carbonated beverages, milk, cream or food relieve the above symptoms", "7. Constipation", "8. Difficulty or pain when swallowing", "9. Black tarry stools", "10. Vomiting blood or vomitus has appearance of coffee-grounds"]
-            <h4>Section 1.3 Small Intestine/Pancreas</h4>
-            ["1. Indigestion, bloating and fullness for several hours after eating", "2. Abdominal cramps or aches", "3. Nausea and/or vomiting", "4. Excessive passage of gas", "5. Diarrhoea (loose, watery or frequent bowel movements)", "6. Constipation (requiring straining, or a hard, dry or small stool)", "7. Alternating constipation and diarrhoea", "8. Undigested food in stools", "9. Stools greasy, smelly or stick to toilet bowl", "10. Black tarry stools", "11. Certain foods worsen abdominal symptoms", "12. Dry flaky skin and dry brittle hair", "13. Difficulty gaining weight"]
-            <h4>Section 1.4 Colon</h4>
-            ["1. Lower abdominal pain, cramping and/or spasms", "2. Lower abdominal pain relieved by passing gas or stool", "3. Excessive gas and bloating", "4. Certain foods or stress aggravate lower abdominal pain", "5. Diarrhoea (loose, watery or frequent bowel movements)", "6. Constipation (requiring straining, or a hard, dry or small stool)", "7. Alternating diarrhoea and constipation", "8. Sensation of incomplete emptying of bowel", "9. Extremely narrow stools", "10. Mucus or pus in stool", "11. Red blood with bowel movement", "12. Rectal pain or cramps", "13. Anal itching"],
-            <h4>Section 1.5 Liver/Gall Bladder/Pancreas</h4>
-            ["1. Upper abdominal pain, or pain under ribs", "2. Bloating or feeling of fullness after eating", "3. Excessive belching or gas", "4. Fatty foods cause indigestion or nausea", "5. Loss of appetite", "6. Nausea and/or vomiting", "7. Unexplained itchy skin", "8. Yellowish discolouration of skin or eyes, or dark coloured urine", "9. Pale clay-coloured stools", "10. Fatigue, malaise or weakness", "11. Fluid retention, oedema", "12. Easy bruising, or bleeding (e.g. of gums)", "13. Loss or thinning of body hair", "14. Red skin, particularly on palms", "15. Dry, flaky skin, or dry hair"]
-          </form>
+          <legend>questionnaire</legend>
+          { this.state.data.headings.map( (heading, i) =>
+                <h3 key={i}>{heading}</h3> &&
+                  ((this.state.data.subheadings[i].length === 0 &&
+                    this.state.data.statements[i].map((statement, _j) =>
+                      <p key={i+_j}>{statement}</p>)) ||
+                    (this.state.data.subheadings[i].map((subheading, j) =>
+                      <h4 key={i + j}>{subheading}</h4> &&
+                      this.state.data.statements[i][j].map((statement, k) =>
+                        <p key={i+j+k}>{statement}</p>))
+                    )
+                  )
+              )
+            }
         </fieldset>
+
       </div>
     )
   }
 }
 
-
-
-// <div id={`item-${i+1}-${j+1}`} className={`section-${i+1}`}>
-//   <label> {`${j+1}. ${s}`} </label>
-//   <input type="radio" />
-//   <input type="radio" />
-//   <input type="radio" />
-//   <input type="radio" />
-// </div>
-
-
-// <div className="pure-control-group pure-u-md-2-5">
-//   <label className="pure-control"> Label 1
-//     <input type="text" className="pure-control" defaultValue="Lorem ipsum" onChange={this.handleChange} />
-//   </label>
-//   <br />
-//   <label className="pure-control"> Label 2
-//     <input type="text" className="pure-control" defaultValue="Lorem 2" onChange={this.handleChange} />
-//   </label>
-// </div>
-// <div className="pure-control-group pure-u-md-2-5">
-//   <label className="pure-control"> Label 3
-//     <input type="text" className="pure-control" defaultValue="Lorem 3" onChange={this.handleChange} />
-//   </label>
-//   <br />
-//   <label className="pure-control"> Label 4
-//     <input type="text" className="pure-control" defaultValue="Lorem 4" onChange={this.handleChange} />
-//   </label>
-// </div>
-// <div className="pure-u-1">
-//   <button type="submit" className="pure-button pure-button-primary pure-control">See Results</button>
-// </div>
+// <fieldset>
+//   <legend>Questionnaire</legend>
+//   <form className="pure-form wellness-form" onSubmit={this.handleSubmit}>
+//     <h3>SECTION 1: GASTROINTESTINAL</h3>
+//     <h4>Section 1.1 Stomach: Hypoacidity</h4>
+//     <h5>1. Indigestion</h5>
+//     <label className="pure-radio">0<input type="radio" checked={this.state.response[0] === 0} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">1<input type="radio" checked={this.state.response[0] === 1} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">2<input type="radio" checked={this.state.response[0] === 2} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">3<input type="radio" checked={this.state.response[0] === 3} onChange={this.handleChange} /></label>
+//     <h5>2. Excessive belching, burping</h5>
+//     <label className="pure-radio">0<input type="radio" checked={this.state.response[1] === 0} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">1<input type="radio" checked={this.state.response[1] === 1} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">2<input type="radio" checked={this.state.response[1] === 2} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">3<input type="radio" checked={this.state.response[1] === 3} onChange={this.handleChange} /></label>
+//     <h5>3. Bloating or fullness commencing during or shortly after a meal</h5>
+//     <label className="pure-radio">0<input type="radio" checked={this.state.response[2] === 0} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">1<input type="radio" checked={this.state.response[2] === 1} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">2<input type="radio" checked={this.state.response[2] === 2} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">3<input type="radio" checked={this.state.response[2] === 3} onChange={this.handleChange} /></label>
+//     <h5>4. Sensation of food sitting in stomach for a prolonged period after a meal</h5>
+//     <label className="pure-radio">0<input type="radio" checked={this.state.response[3] === 0} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">1<input type="radio" checked={this.state.response[3] === 1} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">2<input type="radio" checked={this.state.response[3] === 2} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">3<input type="radio" checked={this.state.response[3] === 3} onChange={this.handleChange} /></label>
+//     <h5>5. Bad breath</h5>
+//     <label className="pure-radio">0<input type="radio" checked={this.state.response[4] === 0} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">1<input type="radio" checked={this.state.response[4] === 1} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">2<input type="radio" checked={this.state.response[4] === 2} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">3<input type="radio" checked={this.state.response[4] === 3} onChange={this.handleChange} /></label>
+//     <h5>6. Loss of appetite, or nausea</h5>
+//     <label className="pure-radio">0<input type="radio" checked={this.state.response[5] === 0} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">1<input type="radio" checked={this.state.response[5] === 1} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">2<input type="radio" checked={this.state.response[5] === 2} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">3<input type="radio" checked={this.state.response[5] === 3} onChange={this.handleChange} /></label>
+//     <h5>7. History of anaemia</h5>
+//     <label className="pure-radio">0<input type="radio" checked={this.state.response[6] === 0} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">1<input type="radio" checked={this.state.response[6] === 1} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">2<input type="radio" checked={this.state.response[6] === 2} onChange={this.handleChange} /></label>
+//     <label className="pure-radio">3<input type="radio" checked={this.state.response[6] === 3} onChange={this.handleChange} /></label>
+//     <h4>Section 1.2 Stomach: Hyperacidity</h4>
+//     ["1. Stomach pain, burning or aching, 1-4 hours after eating", "2. Feeling hungry just an hour or two after eating", "3. Indigestion or heartburn from spicy or fatty food, citrus, alcohol, or caffeine", "4. Stomach discomfort or pain in response to strong emotions, thoughts, or smell of food", "5. Heartburn aggravated by lying down or bending forward", "6. Antacids, carbonated beverages, milk, cream or food relieve the above symptoms", "7. Constipation", "8. Difficulty or pain when swallowing", "9. Black tarry stools", "10. Vomiting blood or vomitus has appearance of coffee-grounds"]
+//     <h4>Section 1.3 Small Intestine/Pancreas</h4>
+//     ["1. Indigestion, bloating and fullness for several hours after eating", "2. Abdominal cramps or aches", "3. Nausea and/or vomiting", "4. Excessive passage of gas", "5. Diarrhoea (loose, watery or frequent bowel movements)", "6. Constipation (requiring straining, or a hard, dry or small stool)", "7. Alternating constipation and diarrhoea", "8. Undigested food in stools", "9. Stools greasy, smelly or stick to toilet bowl", "10. Black tarry stools", "11. Certain foods worsen abdominal symptoms", "12. Dry flaky skin and dry brittle hair", "13. Difficulty gaining weight"]
+//     <h4>Section 1.4 Colon</h4>
+//     ["1. Lower abdominal pain, cramping and/or spasms", "2. Lower abdominal pain relieved by passing gas or stool", "3. Excessive gas and bloating", "4. Certain foods or stress aggravate lower abdominal pain", "5. Diarrhoea (loose, watery or frequent bowel movements)", "6. Constipation (requiring straining, or a hard, dry or small stool)", "7. Alternating diarrhoea and constipation", "8. Sensation of incomplete emptying of bowel", "9. Extremely narrow stools", "10. Mucus or pus in stool", "11. Red blood with bowel movement", "12. Rectal pain or cramps", "13. Anal itching"],
+//     <h4>Section 1.5 Liver/Gall Bladder/Pancreas</h4>
+//     ["1. Upper abdominal pain, or pain under ribs", "2. Bloating or feeling of fullness after eating", "3. Excessive belching or gas", "4. Fatty foods cause indigestion or nausea", "5. Loss of appetite", "6. Nausea and/or vomiting", "7. Unexplained itchy skin", "8. Yellowish discolouration of skin or eyes, or dark coloured urine", "9. Pale clay-coloured stools", "10. Fatigue, malaise or weakness", "11. Fluid retention, oedema", "12. Easy bruising, or bleeding (e.g. of gums)", "13. Loss or thinning of body hair", "14. Red skin, particularly on palms", "15. Dry, flaky skin, or dry hair"]
+//   </form>
+// </fieldset>
