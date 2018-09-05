5052afb3f17f2bc04465dcac6e2dcde3ce1c13b0
mrdev126 <luckman126work@gmail.com>
2018-09-05 00:44:56 -0500

    touchup nav & cleanup nav css

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,30 +10,34 @@ const Header = () => (
         {/*<button className="primary-button pure-button">Compose</button>*/}
         <div className="pure-menu">
           <ul className="pure-menu-list menu-order-1">
-            <li id="home-link" className="pure-menu-item item-order-1">
+            <li id="home-link" className="pure-menu-item">
               <NavLink to='/' className="home-link pure-menu-link">
                 OmniCAT
               </NavLink>
             </li>
-            <li className="pure-menu-heading nav-heading item-order-1">
+            <li>
               <ul className="pure-menu-list menu-order-2">
-                <li className="pure-menu-item item-order-2"><NavLink to='/physical-fitness' className="pure-menu-link">Physical Fitness</NavLink></li>
-                <li className="pure-menu-item item-order-2"><NavLink to='/biochemistry' className="pure-menu-link">Biochemistry</NavLink></li>
-                <ul className="pure-menu-list menu-order-3">
-                  <li className="pure-menu-item item-order-3"><NavLink to='/iron' className="pure-menu-link">Iron Optimiser</NavLink></li>
-                </ul>
-                <li className="pure-menu-item item-order-2"><NavLink to='/brain-health' className="pure-menu-link">Brain Health</NavLink></li>
-                <ul className="pure-menu-list menu-order-3">
-                  <li className="pure-menu-item item-order-3"><NavLink to='/dass42' className="pure-menu-link">Dass42</NavLink></li>
-                </ul>
-                <li className="pure-menu-item item-order-2"><NavLink to='/hormones' className="pure-menu-link">Hormones</NavLink></li>
-                <li className="pure-menu-item item-order-2"><NavLink to='/wellness' className="pure-menu-link">Wellness</NavLink></li>
-                <li className="pure-menu-item item-order-2"><NavLink to='/aesthetics' className="pure-menu-link">Aesthetics</NavLink></li>
-                <li className="pure-menu-item item-order-2"><NavLink to='/risk-and-prevention' className="pure-menu-link">Risk &amp; Prevention</NavLink></li>
+                <li className="pure-menu-item"><NavLink to='/physical-fitness' className="pure-menu-link">Physical Fitness</NavLink></li>
+                <li className="pure-menu-item"><NavLink to='/biochemistry' className="pure-menu-link">Biochemistry</NavLink></li>
+                <li>
+                  <ul className="pure-menu-list menu-order-3">
+                    <li className="pure-menu-item"><NavLink to='/iron' className="pure-menu-link">Iron Optimiser</NavLink></li>
+                  </ul>
+                </li>
+                <li className="pure-menu-item"><NavLink to='/brain-health' className="pure-menu-link">Brain Health</NavLink></li>
+                <li>
+                  <ul className="pure-menu-list menu-order-3">
+                    <li className="pure-menu-item"><NavLink to='/dass42' className="pure-menu-link">Dass42</NavLink></li>
+                  </ul>
+                </li>
+                <li className="pure-menu-item"><NavLink to='/hormones' className="pure-menu-link">Hormones</NavLink></li>
+                <li className="pure-menu-item"><NavLink to='/wellness' className="pure-menu-link">Wellness</NavLink></li>
+                <li className="pure-menu-item"><NavLink to='/aesthetics' className="pure-menu-link">Aesthetics</NavLink></li>
+                <li className="pure-menu-item"><NavLink to='/risk-and-prevention' className="pure-menu-link">Risk &amp; Prevention</NavLink></li>
               </ul>
             </li>
-            <li className="pure-menu-item item-order-1"><NavLink to='/about' className="pure-menu-link">About OmniCAT</NavLink></li>
-            <li className="pure-menu-item item-order-1"><NavLink to='/crowdsale' className="pure-menu-link">Crowdsale</NavLink></li>
+            <li className="pure-menu-item"><NavLink to='/about' className="pure-menu-link">About OmniCAT</NavLink></li>
+            <li className="pure-menu-item"><NavLink to='/crowdsale' className="pure-menu-link">Crowdsale</NavLink></li>
           </ul>
         </div>
       </div>
