b489730b403d6d6b9e5c562f7b07d8c269c3dcbc
happydev829 <happydev000@gmail.com>
2018-09-09 01:41:16 -0500

    work on wellness form, make input nicer looking

--- src/components/Wellness.js
+++ src/components/Wellness.js
@@ -45,31 +45,71 @@ export default class Wellness extends React.Component {
         </div>
         <fieldset>
           <legend>Questionnaire</legend>
+          <form className="pure-form" onSubmit={this.handleSubmit}>
           { headings.map( (heading, i) => (
                 [ <h3 key={i}>{heading}</h3>,
                   i === 10 && <span className="notice-heading">{notices.headings[10]}</span>,
                   subheadings[i].length === 0 ?
                     statements[i][0].map( (statement, j) =>
-                      <p key={j}>{statement}</p>,
+                      <Statement key={j} text={statement} />,
                     ) :
                   subheadings[i].map( (subheading, j) => (
                     [<h4 key={j}>{subheading}</h4>,
                       notices.subheadings[`${i+1}.${j+1}`] && <span className="notice-subheading">{notices.subheadings[`${i+1}.${j+1}`]}</span>,
-                    statements[i][j].map( (statement, k) =>
-                        <p key={k}>
-                          { statement }
-                          {/* FIXME i === 10 && (k === 6 || k === 7) && <p>{notices.statements["11"][`${k+1}`]}</p> */}
-                          <RadioGroup />
-                        </p>
+                    statements[i][j].map( (statement, k) => (
+                          <Statement key={k} text={statement} />
+                        )
                       )
                     ]
                   ))
                 ])
           )}
+          <button type="submit" className="pure-button pure-button-primary">
+            See Results
+          </button>
+          </form>
         </fieldset>
       </div>
     )
   }
+}
+
+class Heading extends React.Component {
+  render() {
+    return(true)
+  }
+}
+
+class SubHeading extends React.Component {
+  render() {
+    return(true)
+  }
+}
+
+class Statement extends React.Component {
+  render() {
+    return(
+      <div key={this.props.key}>
+        <p>{ this.props.text }</p>
+        <RadioGroup />
+      </div>
+    )
+  }
+
+}
+
+class StatementNotice extends React.Component {
+  // FIXME
+  render() {
+    const yup = this.props.heading === 10 && (this.props.statement === 6 || this.props.statement === 7)
+    return (yup)
+    // if (yup) {
+    //   const text = this.props.notices.statements[`11-${this.props.k+1}`]
+    //   const notice = <div><span style={{color: "green"}}>{text}</span></div>
+    // }
+    // return( notice )
+  }
+
 }
 
 class RadioGroup extends React.Component {
@@ -87,23 +127,23 @@ class RadioGroup extends React.Component {
 
   render() {
     return(
-      <span style={{display: 'block', padding: '1em'}}>
+      <span className="component-radio-group">
         <input type="radio"
           checked={false}
           onChange={this.handleChange} />
-        <label>0</label>
+        <label><span>0</span>&nbsp;</label>
         <input type="radio"
           checked={false}
           onChange={this.handleChange} />
-        <label>1</label>
+        <label><span>1</span>&nbsp;</label>
         <input type="radio"
           checked={false}
           onChange={this.handleChange} />
-        <label>2</label>
+        <label><span>2</span>&nbsp;</label>
         <input type="radio"
           checked={false}
           onChange={this.handleChange} />
-        <label>3</label>
+        <label><span>3</span>&nbsp;</label>
       </span>
     )
   }
