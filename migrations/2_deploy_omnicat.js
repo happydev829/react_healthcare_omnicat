4513466c3692f8a8e2db7f28f5d3c345d4cb677e
happydev829 <happydev000@gmail.com>
2018-09-27 20:28:21 -0500

    repair contract compilation, migrations, and tests

--- migrations/2_deploy_omnicat.js
+++ migrations/2_deploy_omnicat.js
@@ -3,7 +3,7 @@ var OmniCAT = artifacts.require("./OmniCAT.sol");
 
 // Make sure Ownable migrates first since OmniCat inherits
 module.exports = function(deployer) {
-  deployer.deploy(Ownable).then((ownableInstance) => {
-    deployer.deploy(OmniCAT);
-  });
-};
+  deployer.deploy(Ownable)
+  deployer.link(Ownable, OmniCAT)
+  deployer.deploy(OmniCAT)
+}
