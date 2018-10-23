21764074f3f98bb0620993024c39c4751730432c
happydev829 <happydev000@gmail.com>
2018-10-22 23:42:49 -0500

    minor changes before next larger context work

--- src/components/Wellness.js
+++ src/components/Wellness.js
@@ -10,11 +10,11 @@ class Wellness extends React.Component {
       data: questionnaire,
       response: [],
       sectionResponse: [],
-      focus: 0, // ([0-9]+|Q)
-      sectionTally: [],
-      complete: [] // ...([0-9]+|Q)
+      focus: 0,    // 0, 1... 11, 'Q', 'results'
+      sectionTally: [], // array up to 13 elements including 'Q' at index[-1]
+      complete: [] // [0, 1... 11, 'Q']
     }
-    // const statements = 407
+    // const statements = 407 + Q?
     this.validate = this.validate.bind(this)
     this.validateText = this.validateText.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -26,7 +26,7 @@ class Wellness extends React.Component {
 
   handleBlur(e) {
     const [ heading, subheading, statement ] = e.target.name.split('-'),
-    textValue = e.target.value
+      textValue = e.target.value
     if (this.validateText(textValue)) {
       this.setState({
         sectionResponse: {
@@ -47,7 +47,6 @@ class Wellness extends React.Component {
       }
     })
     log(JSON.stringify(this.state.sectionResponse))
-    // log(JSON.stringify(this.state.newResponse))
   }
 
   handleSubmit(e) {
@@ -74,7 +73,8 @@ class Wellness extends React.Component {
         sectionTally: this.state.sectionTally.concat(currentSectionTally),
         complete: this.state.complete.concat(sectionFocus),
         focus: nextFocus,
-        response: { ...this.state.response, ...this.state.sectionResponse }
+        response: { ...this.state.response, ...this.state.sectionResponse },
+        sectionResponse: []
       })
       alert('Your tally for the section: ' + currentSectionTally)
       return true
@@ -83,9 +83,11 @@ class Wellness extends React.Component {
       return false
     }
   }
+
   validateText(str) {
     return( str && str.length > 0 )
   }
+
   validate(sectionResponseCount, statementCount) {
     if (sectionResponseCount && statementCount) {
       return sectionResponseCount === statementCount
@@ -95,7 +97,7 @@ class Wellness extends React.Component {
   }
 
   handleQBlur(e) {
-    info(' ## id ', e.target.id, ' ## ', e.target.value)
+    info(' ## id ', e.target.id, ' ## value ', e.target.value)
     this.setState({
       sectionResponse: { ...this.state.sectionResponse, [e.target.id]: e.target.value }
     })
@@ -108,7 +110,8 @@ class Wellness extends React.Component {
       sectionTally: this.state.sectionTally.concat('Q'),
       complete: this.state.complete.concat('Q'),
       focus: 'results',
-      response: { ...this.state.response, ...this.state.sectionResponse }
+      response: { ...this.state.response, ...this.state.sectionResponse },
+      sectionResponse: []
     })
     alert('Thank you for completing this long form.')
     return true
@@ -154,8 +157,8 @@ class Wellness extends React.Component {
 }
 
 const QuestionsForm = props => {
-  const handleSubmit = (e) => props.handleSubmit(e)
-  const handleBlur = (e) => props.handleBlur(e)
+  const handleSubmit = e => props.handleSubmit(e)
+  const handleBlur = e => props.handleBlur(e)
   if (props.focus === 'Q') {
     return(
       <fieldset>
@@ -245,22 +248,20 @@ const SubheadingNotice = props => (
 )
 
 const Statement = props => {
-  // NOTE Add text type not just radio
-  const inputs = props.inputTypeStr // "1 bold ny3", "12 ny6"
+  const inputs = props.inputTypeStr // "1 bold ny3", "12 ny6", "24 0248a", "48 text"
   const id = props.id
   const checked = props.super.state.sectionResponse[id]
   const handleChange = (e) => props.radioChange(e)
   const handleBlur = (e) => props.blur(e)
 
-  // TODO checkbox input (as section 12.4)
   let first, second, third, fourth, yesValue, key1, key2, key3, key4
 
   const bolden   = inputs.includes('bold')
   const textIn   = inputs.includes('text')
   const radioIn2 = inputs.match(/ny(\d{1,2})?/)
   const radioIn4 = inputs.match(/[0-9]{3}a|[0-9]{4}\+?/)
+
   if (radioIn4) {
-    // TODO change label for 0123+ (1, 2, 3, 4) values display above radios
     first  = parseInt(radioIn4[0].slice(0, 1), 16)
     second = parseInt(radioIn4[0].slice(1, 2), 16)
     third  = parseInt(radioIn4[0].slice(2, 3), 16)
@@ -293,7 +294,7 @@ const Statement = props => {
       </div>
     )
   } else if (radioIn2) {
-    return(// TODO
+    return(
       <div className='wellness-statement'>
         <p><span style={{fontWeight: bolden ? 'bold' : 'normal'}}>{props.text}</span></p>
         <div className="pure-control-group">
@@ -306,15 +307,12 @@ const Statement = props => {
     log('r2', radioIn2, ' r4', radioIn4, ' text', textIn, ' \ninputs', inputs)
     return null
   }
-
 }
 
 const RadioButton = props => {
   const handleChange = e => props.radioChange(e)
-  // const keyWidth = props.extraKey ? props.extraKey.length : props.index.toString().length
   return(
     <div className="radio-group">
-
       <input type="radio"
         value={props.name} name={props.name} id={props.name}
         checked={props.checked === props.index}
@@ -327,113 +325,4 @@ const RadioButton = props => {
   )
 }
 
-// const ExtraKey = props => {
-//   if (typeof props.text !== 'undefined') {log(props.text)}
-//   if (typeof props.text === 'string') {
-//     return(
-//       <span style={{position: 'relative', top: '-30px', left: '-30px', float: 'left', clear: 'both'}}>{props.text}</span>
-//     )
-//   } else {
-//     return null
-//   }
-// }
-
 export default Wellness
-// const Statement = props => {
-//   return(
-//     <div id={"statement" + statementID}>
-//       <p>{statement}</p>
-//       <div className="pure-control-group">
-//         <input type="radio"
-//           value={`${statementID}-0`} name={`${statementID}-0`} id={`${statementID}-0`}
-//           checked={this.state.sectionResponse[statementID] === 0}
-//           onChange={this.handleChange}
-//         />
-//         <label htmlFor={`${statementID}-0`}><span>0</span>&nbsp;</label>
-//         <input type="radio"
-//           value={`${statementID}-1`} name={`${statementID}-1`} id={`${statementID}-1`}
-//           checked={this.state.sectionResponse[statementID] === 1}
-//           onChange={this.handleChange}
-//         />
-//         <label htmlFor={`${statementID}-1`}><span>1</span>&nbsp;</label>
-//         <input type="radio"
-//           value={`${statementID}-2`} name={`${statementID}-2`} id={`${statementID}-2`}
-//           checked={this.state.sectionResponse[statementID] === 2}
-//           onChange={this.handleChange}
-//         />
-//         <label htmlFor={`${statementID}-2`}><span>2</span>&nbsp;</label>
-//         <input
-//           type="radio"
-//           value={`${statementID}-3`} name={`${statementID}-3`} id={`${statementID}-3`}
-//           checked={this.state.sectionResponse[statementID] === 3}
-//           onChange={this.handleChange}
-//         />
-//         <label htmlFor={`${statementID++}-3`}><span>3</span>&nbsp;</label>
-//       </div>
-//     </div>
-//   )
-// }
-
-//
-// const Statement = props => {
-//   const handleRadioGroupChange = (id, checked) => {
-//     console.log("In Statement: Called by RadioGroup:", id, checked);
-//     props.onRadioClick(id, checked);
-//   };
-//
-//   return (
-//     <div id={"statement" + props.id}>
-//       <p>{props.text}</p>
-//       // <RadioGroup
-//       //   id={props.id}
-//       //   checkedRadio={props.currentChecked}
-//       //   onRadioChange={handleRadioGroupChange}
-//       // />
-//     </div>
-//   );
-// };
-
-// const RadioGroup = props => {
-//   const handleChange = e => {
-//     let [id, currentChecked] = e.target.value.split('-')
-//     id = parseInt(id, 10)
-//     currentChecked = parseInt(currentChecked, 10)
-//     console.log(
-//       "Going to call parent's method and will give the value:",
-//       id, currentChecked
-//     );
-//     props.onRadioChange(id, currentChecked);
-//   };
-//   return (
-//     <div id={`group-${props.id}`}>
-//       <input
-//         type="radio"
-//         value={`${props.id}-0`}
-//         checked={props.checkedRadio === 0}
-//         onChange={handleChange}
-//       />
-//       <label>0</label>
-//       <input
-//         type="radio"
-//         value={`${props.id}-1`}
-//         checked={props.checkedRadio === 1}
-//         onChange={handleChange}
-//       />
-//       <label>1</label>
-//       <input
-//         type="radio"
-//         value={`${props.id}-2`}
-//         checked={props.checkedRadio === 2}
-//         onChange={handleChange}
-//       />
-//       <label>2</label>
-//       <input
-//         type="radio"
-//         value={`${props.id}-3`}
-//         checked={props.checkedRadio === 3}
-//         onChange={handleChange}
-//       />
-//       <label>3</label>
-//     </div>
-//   );
-// };
