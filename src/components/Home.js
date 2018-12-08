93cad4c53f6c900b449a7c314dcfd636e84bd3b1
happydev829 <happydev000@gmail.com>
2018-12-07 18:55:36 -0600

    shields.io badge to source on gitlab

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ class Home extends React.Component {
     return (
       <div className="homepage">
         <h1>Welcome to OmniCAT</h1>
-        <h2> At Home Ai Guided Biometrics Optimization </h2>
-        <hr />
+        <h2>At Home Ai Guided Biometrics Optimization</h2>
+        <h3><a href="https://gitlab.com/crockett.jesse/omnicat" target="_blank" rel="noopener noreferrer"><img alt="link to source on gitlab" src="https://img.shields.io/badge/open%20source-react%20|%20ethereum-brightgreen.svg?logo=gitlab" /></a></h3>
         <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8R62ecsGXiAUz2Qe6dRixq1yDvCJ3J3cJ0cAIvEJA59Svmw/viewform?embedded=true" width="640" height="687" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
       </div>
     )
