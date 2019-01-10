4bd971a5385d0a54d297139890cd19f9d37fe0c0
happydev829 <happydev000@gmail.com>
2019-01-10 01:31:11 -0600

    homemaking

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,13 +3,13 @@ import React from 'react'
 import { hot } from 'react-hot-loader'
 
 import omniscopeConceptImg from '../images/omniscopeConcept.png'
-import noodleArrowDownRightImg from '../images/noodleArrowDownRight.png'
+import noodleArrowDownRightAImg from '../images/noodleArrowDownRightA.png'
 
 const Products = () => (
   <div>
     <h2>Products</h2>
     <hr />
-    <h3><span style={{ position: 'relative', top: '-0.5rem', left: '-0.6rem' }}><img alt="arrow" src={noodleArrowDownRightImg} /></span>OmniScope Concept</h3>
+    <h3><span style={{ position: 'relative', top: '-0.5rem', left: '-0.6rem' }}><img alt="arrow" src={noodleArrowDownRightAImg} /></span>OmniScope Concept</h3>
     <img alt="omniscope concept illustration" width="100%" src={omniscopeConceptImg} />
   </div>
 )
