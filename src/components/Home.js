9676e06aca2880eb52029df60fb6a157cc28b75a
mrdev126 <luckman126work@gmail.com>
2019-01-27 03:46:48 -0600

    change link from profile badge from gitlab to github & upgrade dev deps

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ const Home = () => (
   <div>
     <h1>Welcome to OmniCAT</h1>
     <h2>At Home Ai Guided Biometrics Optimization</h2>
-    <h3><a href="https://gitlab.com/crockett.jesse/omnicat" target="_blank" rel="noopener noreferrer"><img alt="link to source on github" src="https://img.shields.io/badge/open%20source-react%20|%20ethereum-brightgreen.svg?logo=github&logoColor=white" /></a></h3>
+    <h3>
+      <a href="https://github.com/tidelake/omnicat" target="_blank" rel="noopener noreferrer"><img alt="link to source on github" src="https://img.shields.io/badge/open%20source-react%20|%20ethereum-brightgreen.svg?logo=github&logoColor=white" /></a>
+    </h3>
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8R62ecsGXiAUz2Qe6dRixq1yDvCJ3J3cJ0cAIvEJA59Svmw/viewform?embedded=true" width="640" height="687" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
   </div>
 )
