210588b7045768ffeec8df38a3f9f43b37c52f2b
happydev829 <happydev000@gmail.com>
2019-02-03 03:49:37 -0600

    basic animation on home page headings

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,67 +2,69 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../css/Header.sass'
 
-const Header = () => (
-  <header id="header-content">
-    <nav>
-      <ul className="nav flex-column menu-order-1">
-        <li id="home-link" className="nav-item">
-          <NavLink to='/' className="home-link nav-link">Home</NavLink>
-        </li>
-        <li>
-          <ul className="nav flex-column menu-order-2">
-            <li className="nav-item">
-              <NavLink to='/physical-fitness' className="nav-link">Physical Fitness</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to='/biochemistry' className="nav-link">Biochemistry</NavLink>
-            </li>
-            <li>
-              <ul className="nav flex-column menu-order-3">
-                <li className="nav-item">
-                  <NavLink to='/iron' className="nav-link">Iron Optimiser</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <NavLink to='/brain-health' className="nav-link">Brain Health</NavLink>
-            </li>
-            <li>
-              <ul className="nav flex-column menu-order-3">
-                <li className="nav-item">
-                  <NavLink to='/dass42' className="nav-link">Dass42
-                    <span className="emoji-red-heart">❤️</span></NavLink>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <NavLink to='/hormones' className="nav-link">Hormones</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to='/wellness' className="nav-link">Wellness
-                <span className="emoji-red-heart">❤️</span></NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to='/aesthetics' className="nav-link">Aesthetics
-                <span className="emoji-red-heart">❤️</span></NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to='/risk-and-prevention' className="nav-link">Risk &amp; Prevention</NavLink>
-            </li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/about' className="nav-link">About</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/products' className="nav-link">Products</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to='/crowdsale' className="nav-link">Crowdsale</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  return(
+    <header id="header-content">
+      <nav>
+        <ul className="nav flex-column menu-order-1">
+          <li id="home-link" className="nav-item">
+            <NavLink to='/' className="home-link nav-link">Home</NavLink>
+          </li>
+          <li>
+            <ul className="nav flex-column menu-order-2">
+              <li className="nav-item">
+                <NavLink to='/physical-fitness' className="nav-link">Physical Fitness</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/biochemistry' className="nav-link">Biochemistry</NavLink>
+              </li>
+              <li>
+                <ul className="nav flex-column menu-order-3">
+                  <li className="nav-item">
+                    <NavLink to='/iron' className="nav-link">Iron Optimiser</NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/brain-health' className="nav-link">Brain Health</NavLink>
+              </li>
+              <li>
+                <ul className="nav flex-column menu-order-3">
+                  <li className="nav-item">
+                    <NavLink to='/dass42' className="nav-link">Dass42
+                      <span className="emoji-red-heart">❤️</span></NavLink>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/hormones' className="nav-link">Hormones</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/wellness' className="nav-link">Wellness
+                  <span className="emoji-red-heart">❤️</span></NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/aesthetics' className="nav-link">Aesthetics
+                  <span className="emoji-red-heart">❤️</span></NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to='/risk-and-prevention' className="nav-link">Risk &amp; Prevention</NavLink>
+              </li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <NavLink to='/about' className="nav-link">About</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to='/products' className="nav-link">Products</NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink to='/crowdsale' className="nav-link">Crowdsale</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 export default Header
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 // WAIT import registerServiceWorker from './utils/registerServiceWorker'
 import App from './components/App'
 import './index.sass'
+import './css/animista.scss'
 
 const renderApp = () => render(<Router><App/></Router>, document.getElementById('root'))
 
