129215efb4b3fc5e4afb6e618d434eb6db99e825
happydev829 <happydev000@gmail.com>
2019-02-12 21:10:29 -0600

    add another badge for commit activity

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,12 @@ const Home = () => (
     <h2 className="text-focus-in">At Home Ai Guided Biometrics Optimization</h2>
     <hr id="neatness" />
     <p>
-      <a href="https://github.com/tidelake/omnicat" target="_blank" rel="noopener noreferrer"><img alt="link to source on github" src="https://img.shields.io/badge/open%20source-react%20|%20ethereum-brightgreen.svg?logo=github&logoColor=white" /></a>
+      <a href="https://github.com/tidelake/omnicat" target="_blank" rel="noopener noreferrer">
+        <img alt="link to source on github" src="https://img.shields.io/badge/open%20source-react%20|%20blockchain-brightgreen.svg?logo=github&logoColor=white" />
+      </a> &nbsp; &nbsp;
+      <a href="https://github.com/tidelake/omnicat" target="_blank" rel="noopener noreferrer">
+        <img alt="activity" src="https://img.shields.io/github/commit-activity/m/tidelake/omnicat.svg?logo=github&style=plastic" />
+      </a>
     </p>
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8R62ecsGXiAUz2Qe6dRixq1yDvCJ3J3cJ0cAIvEJA59Svmw/viewform?embedded=true" width="640" height="687" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
   </div>
