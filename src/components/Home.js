3ddb55a5df48b4d84e652d5476889b39318d8e79
happydev829 <happydev000@gmail.com>
2019-02-12 21:24:16 -0600

    add a codacy grade badge

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,11 @@ const Home = () => (
     <p>
       <a href="https://github.com/tidelake/omnicat" target="_blank" rel="noopener noreferrer">
         <img alt="link to source on github" src="https://img.shields.io/badge/open%20source-react%20|%20blockchain-brightgreen.svg?logo=github&logoColor=white" />
-      </a> &nbsp; &nbsp;
+      </a> &nbsp;
       <a href="https://github.com/tidelake/omnicat" target="_blank" rel="noopener noreferrer">
         <img alt="activity" src="https://img.shields.io/github/commit-activity/m/tidelake/omnicat.svg?logo=github&style=plastic" />
+      </a> &nbsp;
+      <a href="https://www.codacy.com/app/tidelake/omnicat?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=tidelake/omnicat&amp;utm_campaign=Badge_Grade" rel="noopener noreferrer" target="_blank"><img alt="codacy grade" src="https://api.codacy.com/project/badge/Grade/0d5f1e7437034b7f99b3aaea705eceb1" />
       </a>
     </p>
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd8R62ecsGXiAUz2Qe6dRixq1yDvCJ3J3cJ0cAIvEJA59Svmw/viewform?embedded=true" width="640" height="687" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
