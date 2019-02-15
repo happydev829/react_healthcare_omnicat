6aa9428ccd5c613e3852c39042af0d68867c2dad
happydev829 <happydev000@gmail.com>
2019-02-15 09:14:25 -0600

    affix nav (works on normal screens md+)

--- src/components/Aesthetics.js
+++ src/components/Aesthetics.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import '../css/Aesthetics.sass'
+import React, { useState } from 'react';
+import '../css/Aesthetics.sass';
 
 const Aesthetics = () => {
   const [keypairs] = useState({
@@ -10,114 +10,180 @@ const Aesthetics = () => {
     selfperception: 50,
     selfconfidence: 50,
     posture: 50,
-    antiageing: 50,
-  })
+    antiageing: 50
+  });
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     // Change slide thumb color on way up
-    if (+e.target.value > +e.target.max * 0.80) {
-      e.target.className = 'range blue'
-    } else if (+e.target.value > +e.target.max * 0.60) {
-      e.target.className = 'range ltpurple'
-    } else if (+e.target.value > +e.target.max * 0.30) {
-      e.target.className = 'range purple'
-    } else if (+e.target.value > 0.00) {
-      e.target.className = 'range pink'
+    if (+e.target.value > +e.target.max * 0.8) {
+      e.target.className = 'range blue';
+    } else if (+e.target.value > +e.target.max * 0.6) {
+      e.target.className = 'range ltpurple';
+    } else if (+e.target.value > +e.target.max * 0.3) {
+      e.target.className = 'range purple';
+    } else if (+e.target.value > 0.0) {
+      e.target.className = 'range pink';
     }
-  }
+  };
 
   // TODO handle focus?
   function handleBlur(e) {
-    keypairs[e.target.name] = +e.target.value
-    console.log(e.target.name, +e.target.value)
+    keypairs[e.target.name] = +e.target.value;
+    console.log(e.target.name, +e.target.value);
   }
 
   function handleSubmit(e) {
-    e.preventDefault()
-    const tally = Object.values(keypairs).reduce((a, b) => a + b, 0)
-    console.log(tally, '/900')
-    alert(`placeholder for tally of ${tally}/900`)
+    e.preventDefault();
+    const tally = Object.values(keypairs).reduce((a, b) => a + b, 0);
+    console.log(tally, '/900');
+    alert(`placeholder for tally of ${tally}/900`);
   }
 
-  return (<div id="aesthetics">
-    <h1 className="text-focus-in">Aesthetics</h1>
-    <p>Beauty, Self Confidence and Awareness, Hygiene, Self Care</p>
-    <hr id="neatness" />
-    <form className="aesthetics" onSubmit={handleSubmit}>
-      <div className="row">
-        <h3>Skin</h3>
-        <p>Skin care and health. Prevention and risk education. Hygiene and routine.</p>
-        <input type="range" className="range blue"
-          onChange={handleChange} onBlur={handleBlur}
-          min={0} max={200} name="skin" />
-        <p>Knowledgeable about skin care and skin cancer prevention. Undergoes routine skin
-          check, practices methods of prevention.
-        </p>
-      </div>
-      <div className="row">
-        <h3>Grooming</h3>
-        <p>Care and health: hair, ears, mouth, nose, lips, face, hands, nails, feet, toes.</p>
-        <input type="range" className="range blue"
-          min={0} max={100} name="grooming"
-          onChange={handleChange} onBlur={handleBlur} />
-        <p>Well groomed: hair, nails, ears, face, hands.</p>
-      </div>
-      <div className="row">
-        <h3>Hygiene</h3>
-        <p>Hygiene education and awareness. Routine and other daily practices.</p>
-        <input type="range" className="range blue"
-          onChange={handleChange} onBlur={handleBlur}
-          min={0} max={100} name="hygiene" />
-        <p>Demonstrates appropriate hygiene practices.</p>
-      </div>
-      <div className="row">
-        <h3>Self care</h3>
-        <p>Overall maintenance, education, identification. Routine.</p>
-        <input type="range" className="range blue"
-          onChange={handleChange} onBlur={handleBlur}
-          min={0} max={100} name="selfcare" />
-        <p>Understands that self-care is important and demonstrates routine.</p>
-      </div>
-      <div className="row">
-        <h3>Self perception</h3>
-        <p>Self awareness. Identification of positive and negative features.</p>
-        <input type="range" className="range blue"
-          onChange={handleChange} onBlur={handleBlur}
-          min={0} max={100} name="selfperception" />
-        <p>Able to perceive oneself realistically. Aware of positive and negative features.</p>
-      </div>
-      <div className="row">
-        <h3>Self confidence</h3>
-        <p>Bravery, courage, determintation. Understanding of own ability.</p>
-        <input type="range" className="range blue"
-          onChange={handleChange} onBlur={handleBlur}
-          min={0} max={100} name="selfconfidence" />
-        <p>Demonstrates confidence in their own ability.</p>
-      </div>
-      <div className="row">
-        <h3>Posture</h3>
-        <p>Spinal alignment. Control, strength and balance.</p>
-        <input type="range" className="range blue"
-          onChange={handleChange} onBlur={handleBlur}
-          min={0} max={100} name="posture" />
-        <p>Shows good posture.</p>
-      </div>
-      <div className="row">
-        <h3>Anti-Ageing</h3>
-        <p>Education and awareness. Routine and other daily practices.</p>
-        <input type="range" className="range blue"
-          onChange={handleChange} onBlur={handleBlur}
-          min={0} max={100} name="antiageing" />
-        <p>Knowledgeable in regard to benefits of anti-ageing practices.
-          Practices anti-ageing measures.
-        </p>
-      </div>
-      <button type="submit" className="pure-button pure-button-primary">
-        See Results (pending/use live tabulation and then commit result)
-      </button>
-    </form>
-  </div>
-  )
-}
+  return (
+    <div id="aesthetics">
+      <h1 className="text-focus-in">Aesthetics</h1>
+      <p>Beauty, Self Confidence and Awareness, Hygiene, Self Care</p>
+      <hr id="neatness" />
+      <form className="aesthetics" onSubmit={handleSubmit}>
+        <div className="row">
+          <h3>Skin</h3>
+          <p>
+            Skin care and health. Prevention and risk education. Hygiene and
+            routine.
+          </p>
+          <input
+            type="range"
+            className="range blue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            min={0}
+            max={200}
+            name="skin"
+          />
+          <p>
+            Knowledgeable about skin care and skin cancer prevention. Undergoes
+            routine skin check, practices methods of prevention.
+          </p>
+        </div>
+        <div className="row">
+          <h3>Grooming</h3>
+          <p>
+            Care and health: hair, ears, mouth, nose, lips, face, hands, nails,
+            feet, toes.
+          </p>
+          <input
+            type="range"
+            className="range blue"
+            min={0}
+            max={100}
+            name="grooming"
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          <p>Well groomed: hair, nails, ears, face, hands.</p>
+        </div>
+        <div className="row">
+          <h3>Hygiene</h3>
+          <p>
+            Hygiene education and awareness. Routine and other daily practices.
+          </p>
+          <input
+            type="range"
+            className="range blue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            min={0}
+            max={100}
+            name="hygiene"
+          />
+          <p>Demonstrates appropriate hygiene practices.</p>
+        </div>
+        <div className="row">
+          <h3>Self care</h3>
+          <p>Overall maintenance, education, identification. Routine.</p>
+          <input
+            type="range"
+            className="range blue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            min={0}
+            max={100}
+            name="selfcare"
+          />
+          <p>
+            Understands that self-care is important and demonstrates routine.
+          </p>
+        </div>
+        <div className="row">
+          <h3>Self perception</h3>
+          <p>
+            Self awareness. Identification of positive and negative features.
+          </p>
+          <input
+            type="range"
+            className="range blue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            min={0}
+            max={100}
+            name="selfperception"
+          />
+          <p>
+            Able to perceive oneself realistically. Aware of positive and
+            negative features.
+          </p>
+        </div>
+        <div className="row">
+          <h3>Self confidence</h3>
+          <p>Bravery, courage, determintation. Understanding of own ability.</p>
+          <input
+            type="range"
+            className="range blue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            min={0}
+            max={100}
+            name="selfconfidence"
+          />
+          <p>Demonstrates confidence in their own ability.</p>
+        </div>
+        <div className="row">
+          <h3>Posture</h3>
+          <p>Spinal alignment. Control, strength and balance.</p>
+          <input
+            type="range"
+            className="range blue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            min={0}
+            max={100}
+            name="posture"
+          />
+          <p>Shows good posture.</p>
+        </div>
+        <div className="row">
+          <h3>Anti-Ageing</h3>
+          <p>Education and awareness. Routine and other daily practices.</p>
+          <input
+            type="range"
+            className="range blue"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            min={0}
+            max={100}
+            name="antiageing"
+          />
+          <p>
+            Knowledgeable in regard to benefits of anti-ageing practices.
+            Practices anti-ageing measures.
+          </p>
+        </div>
+        <button type="submit" className="pure-button pure-button-primary">
+          See Results (pending/use live tabulation and then commit result)
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Aesthetics
+export default Aesthetics;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,14 +1,23 @@
-import React from 'react'
-import omniscopeConceptImg from '../images/omniscopeConcept.png'
-import noodleArrowDownRightAImg from '../images/noodleArrowDownRightA.png'
+import React from 'react';
+import omniscopeConceptImg from '../images/omniscopeConcept.png';
+import noodleArrowDownRightAImg from '../images/noodleArrowDownRightA.png';
 
 const Products = () => (
   <div>
     <h1 className="text-focus-in">Products</h1>
     <hr id="neatness" />
-    <h3 className="text-focus-in"><span style={{ position: 'relative', top: '-0.5rem', left: '-0.6rem' }}><img alt="arrow" src={noodleArrowDownRightAImg} /></span>OmniScope Concept</h3>
-    <img alt="omniscope concept illustration" width="100%" src={omniscopeConceptImg} />
+    <h3 className="text-focus-in">
+      <span style={{ position: 'relative', top: '-0.5rem', left: '-0.6rem' }}>
+        <img alt="arrow" src={noodleArrowDownRightAImg} />
+      </span>
+      OmniScope Concept
+    </h3>
+    <img
+      alt="omniscope concept illustration"
+      width="100%"
+      src={omniscopeConceptImg}
+    />
   </div>
-)
+);
 
-export default Products
+export default Products;
