e12784a4b12c3df0e3210e7cb4f245b2cac48404
mrdev126 <luckman126work@gmail.com>
2019-02-15 09:14:25 -0600

    affix nav (works on normal screens md+)

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => (
   <div className="about">
@@ -7,83 +7,100 @@ const About = () => (
     <hr id="neatness" />
     <h3>Overview</h3>
 
-    <p>OmniCAT is a library of clinical assessment tools and resources intended to empower
-      patients to record, monitor and optimize personal health metrics with a high degree of
-      proficiency, with or without the support of a qualified medical practitioner.
+    <p>
+      OmniCAT is a library of clinical assessment tools and resources intended
+      to empower patients to record, monitor and optimize personal health
+      metrics with a high degree of proficiency, with or without the support of
+      a qualified medical practitioner.
     </p>
 
-    <p>Additionally, it allows providers to collate, analyse and optimize patients health
-      metrics with a high degree of proficiency for support in diagnosis, patient management
-      and treatment protocol implementation and supports access to distributed data systems
-      and open secure multimedia communications with colleagues.
+    <p>
+      Additionally, it allows providers to collate, analyse and optimize
+      patients health metrics with a high degree of proficiency for support in
+      diagnosis, patient management and treatment protocol implementation and
+      supports access to distributed data systems and open secure multimedia
+      communications with colleagues.
     </p>
 
-    <p>Furthermore, the OmniCAT platform will provide greater accuracy in interpretation and
-      diagnosis and increased success of treatment and management protocols, reduced costs of
-      health care, improved patient engagement, awareness and outcomes for preventative measures
-      as well as providing more substantial transparency.
+    <p>
+      Furthermore, the OmniCAT platform will provide greater accuracy in
+      interpretation and diagnosis and increased success of treatment and
+      management protocols, reduced costs of health care, improved patient
+      engagement, awareness and outcomes for preventative measures as well as
+      providing more substantial transparency.
     </p>
 
     <h3>Vision and Goal</h3>
 
-    <p>Our vision is to contribute meaningfully to the proliferation of knowledge and
-      education in preparation for the advent of emerging medical and healthcare technologies,
-      treatments and therapies.
+    <p>
+      Our vision is to contribute meaningfully to the proliferation of knowledge
+      and education in preparation for the advent of emerging medical and
+      healthcare technologies, treatments and therapies.
     </p>
 
-    <p>Our goal is to influence a fundamental shift in the perception of medical data
-      interpretation from the present paradigm to one which focuses on achieving optimal
-      targets rather than normal or acceptable.
+    <p>
+      Our goal is to influence a fundamental shift in the perception of medical
+      data interpretation from the present paradigm to one which focuses on
+      achieving optimal targets rather than normal or acceptable.
     </p>
 
     <h3>The Problem</h3>
 
-    <p>National healthcare systems are broken - Reduce costs, improve diagnostic accuracy,
-      increase patient engagement and awareness, enhance relevancy of medical data. Ensure
-      sustainability.
+    <p>
+      National healthcare systems are broken - Reduce costs, improve diagnostic
+      accuracy, increase patient engagement and awareness, enhance relevancy of
+      medical data. Ensure sustainability.
     </p>
 
-    <p>Every Dr is an island - Connect healthcare providers to each other; connect the
-      patients to the health care providers. Decentralise the data.
+    <p>
+      Every Dr is an island - Connect healthcare providers to each other;
+      connect the patients to the health care providers. Decentralise the data.
     </p>
 
     <h3>Objectives</h3>
 
-    <p>For the Patient - Record, monitor and optimise personal health metrics with a high
-      degree of proficiency, with or without the support of a qualified medical practitioner.
+    <p>
+      For the Patient - Record, monitor and optimise personal health metrics
+      with a high degree of proficiency, with or without the support of a
+      qualified medical practitioner.
     </p>
 
-    <p>For the Provider - Collate, analyse and optimise patients health metrics with a high
-      degree of proficiency for support in diagnosis, patient management and treatment protocol
-      implementation. Access to distributed data systems and open secure multimedia
-      communications with colleagues.
+    <p>
+      For the Provider - Collate, analyse and optimise patients health metrics
+      with a high degree of proficiency for support in diagnosis, patient
+      management and treatment protocol implementation. Access to distributed
+      data systems and open secure multimedia communications with colleagues.
     </p>
 
-    <p>For Organisations and Governments - Allow for greater accuracy in diagnosis and
-      increased success of treatment and management protocols. Reduced costs of healthcare.
-      Improved patient engagement and awareness and outcomes for preventative measures.
-      More substantial transparency.
+    <p>
+      For Organisations and Governments - Allow for greater accuracy in
+      diagnosis and increased success of treatment and management protocols.
+      Reduced costs of healthcare. Improved patient engagement and awareness and
+      outcomes for preventative measures. More substantial transparency.
     </p>
 
     <h3>Scope</h3>
 
-    <p>Physical Fitness; Biochemistry; Brain Health; Wellness; Aesthetics; Hormonal Health;
-      Risk and Prevention
+    <p>
+      Physical Fitness; Biochemistry; Brain Health; Wellness; Aesthetics;
+      Hormonal Health; Risk and Prevention
     </p>
 
     <h3>Usability Objectives</h3>
 
-    <p>Enter medical data and read interpretation as relevant to the parameters provided
-      by the Optimal Health Dataset.
+    <p>
+      Enter medical data and read interpretation as relevant to the parameters
+      provided by the Optimal Health Dataset.
     </p>
 
     <h3>Powered by Ethereum</h3>
 
-    <p>OmniCAT uses smart contracts and the Ethereum blockchain to execute the logic
-      of various clinical tools and assessments. This ensures transperency of our methods
-      and enhances the proliferation of our open source materials and allows us to leverage
-      the advantages of the ethereum virtual machine and its underlying distributed ledger
-      technology.
+    <p>
+      OmniCAT uses smart contracts and the Ethereum blockchain to execute the
+      logic of various clinical tools and assessments. This ensures transperency
+      of our methods and enhances the proliferation of our open source materials
+      and allows us to leverage the advantages of the ethereum virtual machine
+      and its underlying distributed ledger technology.
     </p>
 
     <h3>Website</h3>
@@ -92,8 +109,9 @@ const About = () => (
 
     <h3>Contributing</h3>
 
-    <p>OmniCAT is an open source project and we are very happy to accept community
-      contributions.
+    <p>
+      OmniCAT is an open source project and we are very happy to accept
+      community contributions.
     </p>
 
     <h3>Licensing</h3>
@@ -108,5 +126,5 @@ const About = () => (
 
     <h3>Documentation</h3>
   </div>
-)
-export default About
+);
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import Footer from './Footer'
 import '../css/App.sass'
 import neatness from '../utils/neatness'
 
-function App() {
+const App = () => {
   useEffect(() => {
     neatness(document.getElementById('neatness'))
   })
@@ -33,39 +33,3 @@ function App() {
 }
 
 export default App
-
-//
-// function neatnav() {
-//   const borderColorOpacity = 1,
-//     borderColor1 = `rgba(249,32,86, ${borderColorOpacity})`,
-//     borderColor2 = `rgba(0,222,255, ${borderColorOpacity})`,
-//     header = document.getElementById('neatness')
-//
-//   let i = 0, k = 30, l = 60
-//
-//   function getNextShadowPosition(shadow, offset){
-//     shadow.x = -Math.cos((i + offset)/10) * 10 ;
-//     shadow.y = -Math.sin((i + offset)/10) * 10;
-//   }
-//
-//   function shadowToString() {
-//     getNextShadowPosition(this, this.shadowOffset);
-//
-//     return this.x + 'px ' + this.y + 'px ' + this.blured + 'px ' + this.color
-//   }
-//
-//   function showAndMove() {
-//     showAndMove.timer = setTimeout(function f() {
-//
-//       //change border
-//       header.style.borderImageRadius = '2em'
-//       header.style.borderImage = `linear-gradient(${i}deg, ${borderColor1} ${k}%, ${borderColor2} ${l}%)`
-//       i++
-//
-//       //cycle
-//       showAndMove.timer = setTimeout(f, 50)
-//     }, 0)
-//   }
-//
-//   return showAndMove()
-// }
