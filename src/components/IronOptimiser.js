c32453e66d588a04642281d83d24129344c75ea8
happydev829 <happydev000@gmail.com>
2019-02-16 10:28:37 -0600

    color code Iron module results

--- src/components/IronOptimiser.js
+++ src/components/IronOptimiser.js
@@ -32,11 +32,16 @@ const IronOptimiser = () => {
     }
   }
 
+  React.useEffect(() => {
+    console.log(results)
+  })
+
   const handleSubmit = event => {
     event.preventDefault()
     if (validate()) {
       console.log('valid')
       tallyResults()
+      console.log(results)
       return true
     } else {
       alert('Please provide values for all 4')
@@ -72,9 +77,26 @@ const IronOptimiser = () => {
          sfa >= 181 && sfa <= 290 ? 'Suboptimal high' :
          sfa < 20 ? 'Abnormal low' : 'Abnormal high') + ' Serum Ferritin Assay levels'
 
-    setResults({ si: report_si, ti: report_ti, ts: report_ts, sfa: report_sfa })
-  }
+    const siColor = setColorCodes(report_si)
+    const tiColor = setColorCodes(report_ti)
+    const tsColor = setColorCodes(report_ts)
+    const sfaColor = setColorCodes(report_sfa)
 
+    setResults({
+      si: report_si, ti: report_ti, ts: report_ts, sfa: report_sfa,
+      siPct: (100.0 * si / 20.0).toFixed(0),
+      tiPct: (100.0 * ti / 60.0).toFixed(0),
+      tsPct: (100.0 * ts / 40.0).toFixed(0),
+      sfaPct: (100.0 * sfa / 150.0).toFixed(0),
+      color_si: siColor, color_ti: tiColor, color_ts: tsColor, color_sfa: sfaColor
+    })
+  }
+  const setColorCodes = (text) => {
+    return text.match('Optimal') ? 'bg-success text-white '
+        : text.match('Normal') ? 'bg-primary text-white '
+        : text.match('Suboptimal') ? 'bg-warning text-dark '
+        : 'bg-danger text-white '
+  }
   return (
     <div className="iron row">
       <h1 className="text-focus-in">Iron Optimiser</h1>
@@ -82,25 +104,33 @@ const IronOptimiser = () => {
       <form className="col-sm-12 mt-4" onSubmit={handleSubmit}>
         <h4> Enter values for the following instances </h4>
         <div className="card">
-          { results && <div>{results.si}</div> }
+          { results && <div>{results.si}<br />
+            <p className={results.color_si+' d-inline-flex p-2 m-1 bd-highlight'}>
+              {results.siPct}% of optimal</p></div> }
           <label htmlFor="serumIron"> Serum Iron
             <input type="number" min="0" max="999" name="serumIron" onChange={handleChange} />
           </label>
         </div>
         <div className="card">
-          { results && <div>{results.ti}</div> }
+          { results && <div>{results.ti}<br />
+            <p className={results.color_ti+' d-inline-flex p-2 m-1 bd-highlight'}>
+              {results.tiPct}% of optimal</p></div> }
           <label htmlFor="transferrinIBC"> TransferrinIBC
             <input type="number" min="0" max="999" name="transferrinIBC" onChange={handleChange} />
           </label>
         </div>
         <div className="card">
-          { results && <div>{results.ts}</div> }
+          { results && <div>{results.ts}<br />
+            <p className={results.color_ts+' d-inline-flex p-2 m-1 bd-highlight'}>
+              {results.tsPct}% of optimal</p></div> }
           <label htmlFor="transferrinSaturation"> Transferrin Saturation
             <input type="number" min="0" max="999" name="transferrinSaturation" onChange={handleChange} />
           </label>
         </div>
         <div className="card">
-          { results && <div>{results.sfa}</div> }
+          { results && <div>{results.sfa}<br />
+            <p className={results.color_sfa+' d-inline-flex p-2 m-1 bd-highlight'}>
+              {results.sfaPct}% of optimal</p></div> }
           <label htmlFor="serumFerritinAssay"> Serum Ferritin Assay
             <input type="number" min="0" max="999" name="serumFerritinAssay" onChange={handleChange} />
           </label>
