f974ccc209d91e74acca40f4b116e16fda54d9ad
happydev829 <happydev000@gmail.com>
2019-03-06 19:58:19 -0600

    tune linting and populate wellness module from localstorage

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import '/index.sass'
-import '/scss/animista.scss'
-import App from '/components/App'
-import * as serviceWorker from '/utils/serviceWorker'
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import "./index.sass";
+import "./scss/animista.scss";
+import App from "./components/App";
+import * as serviceWorker from "./utils/serviceWorker";
 
-const renderApp = () => render(<Router><App /></Router>, document.getElementById('root'))
+const renderApp = () =>
+  render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById("root")
+  );
 
-renderApp()
+renderApp();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
