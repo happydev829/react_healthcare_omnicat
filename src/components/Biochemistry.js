c8b360659be2cd8f463000aa9b1a21cb3cba1320
mrdev126 <luckman126work@gmail.com>
2019-04-23 12:57:30 -0500

    CssButton component basics

--- src/components/Biochemistry.js
+++ src/components/Biochemistry.js
@@ -1,11 +1,18 @@
 import React from "react";
-import CssButtons from "./CssButtons";
+import StyledButton from "./StyledButton";
 
 const Biochemistry = () => (
   <div>
     <h1 className="text-focus-in">Biochemistry</h1>
     <hr id="neatness" />
-    <CssButtons />
+    <StyledButton type="back" text="BACK" />
+    <StyledButton type="next" text="NEXT" />
+    <StyledButton type="angle" text="angle" />
+    <StyledButton type="anglefill" text="anglefill" />
+    <StyledButton type="plus" text="plus" />{" "}
+    <StyledButton type="email" text="email" />
+    <StyledButton type="settings" text="settings" />
+    <StyledButton type="search" text="search" />
   </div>
 );
 
