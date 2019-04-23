c83ce46de93dfb8cc7aee70317afe47418af062b
mrdev126 <luckman126work@gmail.com>
2019-04-23 13:07:28 -0500

    place element components in src/components/items

--- src/components/Biochemistry.js
+++ src/components/Biochemistry.js
@@ -1,5 +1,5 @@
 import React from "react";
-import StyledButton from "./StyledButton";
+import StyledButton from "./items/StyledButton";
 
 const Biochemistry = () => (
   <div>
--- src/components/items/StyledButton.js
+++ src/components/items/StyledButton.js
@@ -1,6 +1,5 @@
 import React from "react";
-import "./../scss/StyledButton.sass";
-// import { Z_PARTIAL_FLUSH } from "zlib";
+import "./../../scss/StyledButton.sass";
 
 const anglefill = function(text = " ") {
   return (
@@ -195,23 +194,30 @@ const settings = function(text = "SETTINGS") {
   );
 };
 
-const search = function (text = 'Search') {
+const search = function(text = "Search") {
   return (
     <div id="css-button">
       <div class="standard flex color-bright">
         <div class="round flex">
-          <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M24.2013 22.771L18.2512 16.5827C19.7811 14.7641 20.6193 12.4759 20.6193 10.0937C20.6193 4.52814 16.0912 0 10.5256 0C4.95996 0 0.431824 4.52814 0.431824 10.0937C0.431824 15.6593 4.95996 20.1875 10.5256 20.1875C12.615 20.1875 14.6061 19.5573 16.3084 18.361L22.3036 24.5963C22.5542 24.8565 22.8913 25 23.2525 25C23.5943 25 23.9186 24.8697 24.1648 24.6327C24.688 24.1293 24.7046 23.2946 24.2013 22.771ZM10.5256 2.63315C14.6394 2.63315 17.9862 5.97988 17.9862 10.0937C17.9862 14.2076 14.6394 17.5543 10.5256 17.5543C6.41171 17.5543 3.06497 14.2076 3.06497 10.0937C3.06497 5.97988 6.41171 2.63315 10.5256 2.63315Z" fill="#16a085"/>
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 25 25"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M24.2013 22.771L18.2512 16.5827C19.7811 14.7641 20.6193 12.4759 20.6193 10.0937C20.6193 4.52814 16.0912 0 10.5256 0C4.95996 0 0.431824 4.52814 0.431824 10.0937C0.431824 15.6593 4.95996 20.1875 10.5256 20.1875C12.615 20.1875 14.6061 19.5573 16.3084 18.361L22.3036 24.5963C22.5542 24.8565 22.8913 25 23.2525 25C23.5943 25 23.9186 24.8697 24.1648 24.6327C24.688 24.1293 24.7046 23.2946 24.2013 22.771ZM10.5256 2.63315C14.6394 2.63315 17.9862 5.97988 17.9862 10.0937C17.9862 14.2076 14.6394 17.5543 10.5256 17.5543C6.41171 17.5543 3.06497 14.2076 3.06497 10.0937C3.06497 5.97988 6.41171 2.63315 10.5256 2.63315Z"
+              fill="#16a085"
+            />
           </svg>
         </div>
-        <div class="button flex up">
-          {text}
-        </div>
+        <div class="button flex up">{text}</div>
         <div class="button" />
       </div>
     </div>
-  )
-}
+  );
+};
 
 const StyledButton = props => {
   return props.type === "anglefill" ? (
@@ -230,13 +236,15 @@ const StyledButton = props => {
     settings(props.text)
   ) : props.type === "search" ? (
     search(props.text)
-  ) :  <h4>whoops... {props}</h4>
-
+  ) : (
+    <h4>whoops... {props}</h4>
+  );
 };
 
 export default StyledButton;
 
 // #################################################
+//         Other Types see https://codepen.io/tidelake/pen/dLqEYJ
 /*
 
 <div className="save-button color-bright flex">
