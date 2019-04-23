d34f3232c1fd3737c359cadf97d292a64949e339
mrdev126 <luckman126work@gmail.com>
2019-04-23 10:27:30 -0500

    pretty buttons on Biochemistry page

--- src/components/Biochemistry.js
+++ src/components/Biochemistry.js
@@ -1,9 +1,11 @@
 import React from "react";
+import CssButtons from "./CssButtons";
 
 const Biochemistry = () => (
   <div>
     <h1 className="text-focus-in">Biochemistry</h1>
     <hr id="neatness" />
+    <CssButtons />
   </div>
 );
 
